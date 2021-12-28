3e0cce5323d12a6de73188d3b280383f2f4711ee
WillGig <willgiguere@gmail.com>
2021-12-28 14:52:51 -0500

    adjusted texture loading

--- Chess/src/utils/SpriteSheet.java
+++ Chess/src/utils/SpriteSheet.java
@@ -13,17 +13,9 @@ public class SpriteSheet {
 	
 	public Texture load(int xOff, int yOff){
 		int[] data = new int[spriteWidth*spriteHeight];
-		for(int y = 0; y < spriteHeight; y++){
-			for(int x = 0; x < spriteWidth; x++){
-				int a = (image.pixels[x+xOff*spriteWidth+(y+yOff*spriteHeight)*image.width] & 0xff000000) >> 24;
-				int r = (image.pixels[x+xOff*spriteWidth+(y+yOff*spriteHeight)*image.width] & 0xff0000) >> 16;
-				int g = (image.pixels[x+xOff*spriteWidth+(y+yOff*spriteHeight)*image.width] & 0xff00) >> 8;
-				int b = (image.pixels[x+xOff*spriteWidth+(y+yOff*spriteHeight)*image.width] & 0xff);
-				data[x + y * spriteWidth] = a << 24 | b << 16 | g << 8 | r; 
-				
-//				data[x + y * spriteWidth] = image.pixels[x + xOff*spriteWidth + (y+yOff*spriteHeight) * image.width];
-			}
-		}
+		for(int y = 0; y < spriteHeight; y++)
+			for(int x = 0; x < spriteWidth; x++)
+				data[x + y * spriteWidth] = image.pixels[x + xOff*spriteWidth + (y+yOff*spriteHeight) * image.width];
 		
 		return new Texture(spriteWidth, spriteHeight, data);
 	}
